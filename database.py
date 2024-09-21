c583194432c0eafc3f57cf2de392e9498e4eee7d
leehai1107 <leehai1107@gmail.com>
2024-09-21 23:31:00 +0700

    update:add env

--- database.py
+++ database.py
@@ -1,8 +1,14 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URL = "postgresql://admin201exe:12345678@database-exe201.cxcsqkseiybf.ap-southeast-1.rds.amazonaws.com:5432/spotlight.v3"  # Hoặc sử dụng PostgreSQL, MySQL
+load_dotenv()
+
+database_url = os.getenv('DATABASE_URL')
+
+SQLALCHEMY_DATABASE_URL = database_url  # Hoặc sử dụng PostgreSQL, MySQL
 
 # Tạo engine kết nối với cơ sở dữ liệu PostgreSQL (loại bỏ check_same_thread)
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
