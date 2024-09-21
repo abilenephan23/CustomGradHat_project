8219c5fe5233ff36239a76b681f242b28d8bdcf3
Abilene Phan <phan.le.thao.231@gmail.com>
2024-09-21 21:03:21 +0700

    update: link to PostgreSQL instead of SQLite

--- database.py
+++ database.py
@@ -2,12 +2,18 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./test.db"  # Hoặc sử dụng PostgreSQL, MySQL
+SQLALCHEMY_DATABASE_URL = "postgresql://admin201exe:12345678@database-exe201.cxcsqkseiybf.ap-southeast-1.rds.amazonaws.com:5432/spotlight"  # Hoặc sử dụng PostgreSQL, MySQL
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Tạo engine kết nối với cơ sở dữ liệu PostgreSQL (loại bỏ check_same_thread)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# Tạo session để tương tác với cơ sở dữ liệu
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Tạo lớp cơ sở cho các models
 Base = declarative_base()
 
+# Dependency để sử dụng session
 def get_db():
     db = SessionLocal()
     try:
