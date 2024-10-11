f25af48c753b7b3181afa80e9114f251cc40ff82
Le Chi Hai <leehai1107@gmail.com>
2024-10-11 21:42:27 +0700

    Thao plt (#6)
    * 1st version of backend page
    
    * update: link to PostgreSQL instead of SQLite
    
    * change link database
    
    * update: fix api
    
    * update: update gitignore
    
    * update
    
    * update
    
    * update: for deployment
    
    * update:add env
    
    * update: fix api sign up and sign in
    
    * fix: sign up shop if phone exist
    
    * update: enable cors
    
    * update: response to jwt
    
    * update:update jwt payload
    
    * cập nhật API duyệt account theo Username
    
    * cập nhật thêm biến status (boolean)
    
    * update: checking
    
    * update: hot fix
    
    * API tạo category và sản phẩm theo category
    
    * update:fix status
    
    * update: api tạo category.
    issue: api tạo item chưa xác định được nên filter theo cái nào
    
    * update: api thêm item theo category_id và shop_id
    
    * update: fix status for shop
    
    * update: api lấy thông tin sản phẩm cụ thể theo id + api lấy tất cả sản phẩm phân trang
    
    * update: API get all users pagination
    
    * update: fix data response
    
    * update: API get all item
    
    * sửa lại tên trong phần return
    
    * update: update sigin
    
    * API lấy tất cả sản phẩm của chủ shop (phân trang)
    
    * fix: hot fix
    
    * update: fix create item
    
    * update: add quantity
    
    * api customize
    
    * update: add more fields data
    
    * update: add get item by id and implement order APIs (#5)
    
    ---------
    
    Co-authored-by: Abilene Phan <phan.le.thao.231@gmail.com>

--- models/order.py
+++ models/order.py
@@ -8,17 +8,30 @@ class Order(Base):
        __tablename__ = "orders"
        order_id = Column(Integer, primary_key=True, index=True)
        customer_id = Column(Integer, ForeignKey("users.user_id"))
-       quantity = Column(Integer)
        total_price = Column(Integer)
        order_at = Column(DateTime(timezone=True), server_default=func.now())
        order_status = Column(BIT(1))
-       payment_id = Column(Integer, ForeignKey("payments.payment_id"))
        response = Column(String)
-       img_url = Column(String)
-       variation_id = Column(Integer, ForeignKey("item_variations.variation_id"))
-       customization_id = Column(Integer, ForeignKey("customizations.customization_id"))
+       customer_address = Column(String)
+       customer_name = Column(String)
+       customer_phone = Column(String)
+       image_url = Column(String)
+       payment_status = Column(BIT(1))
+       shipping_status = Column(BIT(1))
 
        user = relationship("User", back_populates="orders")
-       item_variation = relationship("ItemVariation", back_populates="orders")
-       customization = relationship("Customization", back_populates="orders")
-       payment = relationship("Payment", back_populates="orders")
+       details = relationship("OrderDetails", back_populates="order")
+
+class OrderDetails(Base):
+       __tablename__ = "order_details"
+       order_details_id = Column(Integer, primary_key=True, index=True)
+       order_id = Column(Integer, ForeignKey("orders.order_id"))
+       item_id = Column(Integer, ForeignKey("items.item_id"))
+       customization_id = Column(Integer, ForeignKey("customizations.customization_id"))
+       item_quantity = Column(Integer)
+       color_id = Column(Integer, ForeignKey("colors.color_id"))
+       size_id = Column(Integer, ForeignKey("sizes.size_id"))
+
+       order = relationship("Order", back_populates="details")
+       item = relationship("Item", back_populates="details")
+       customization = relationship("Customization", back_populates="details")
